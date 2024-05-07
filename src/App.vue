faf68f1742a61829f803bd6417996ca2f41dc9b6
aldoaquino451 <aldoaquino2909@gmail.com>
2024-05-07 12:34:30 +0200

    [init] Currency, Search component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,97 @@
 <script>
+import axios from "axios";
+import Search from "./components/Search.vue";
+import Currency from "./components/Currency.vue";
+
+export default {
+  components: {
+    Search,
+    Currency,
+  },
+
+  data() {
+    return {
+      baseUrl: "https://api.frankfurter.app/latest",
+      amount: 1,
+      from: 'EUR',
+      to: 'USD',
+      currencyResult: {}, 
+      currencies: {},
+    }
+  },
+
+  methods: {
+    getCurrencies() {
+      axios.get('https://api.frankfurter.app/currencies')
+        .then( res => {
+          this.currencies = res.data;
+        })
+    },
+
+    getConvertion(amount,from,to) {
+      axios.get(this.baseUrl, { params: {
+        amount: amount,
+        from: from,
+        to: to
+      }}) 
+        .then( res => {
+          this.currencyResult = res.data;
+
+          const rates = Object.entries(this.currencyResult.rates);
+
+          // rates.forEach((currency, key) => {
+          //   console.log(currency[0], currency[1]);
+          // });
+          
+          for (const [key, value] of Object.entries(this.currencyResult.rates)) {
+            this.currencyResult['baseFinal'] = key;
+            this.currencyResult['amountFinal'] = value;
+          }
+        })
+    }  
+
+  },
+
+  mounted() {
+    this.getCurrencies(); 
+    this.getConvertion(1, 'EUR', 'USD');
+  },
+}
 
 
 </script>
 
 <template>
 
-  <p class="blue">ciao mondo</p>
+  <div class="my-container">
+
+    <h1 class="text-center">Currency Converter</h1>
+
+    <div>
+      <Currency :currencyResult="this.currencyResult" />
+    </div>
+
+    <div class="d-flex flex-column gap-3">
+      <Search :currencies="this.currencies" />
+      <Search :currencies="this.currencies" />
+    </div>
+
+  </div>
 
 </template>
 
 
 
-<style lang="sass">
+<style lang="scss">
+
+.my-container {
+  max-width: 1000px;
+  width: 70%;
+  margin: 150px auto;
+  min-height: 500px;
+  border: 1px solid black;
+  border-radius: 40px;
+  padding: 50px;
+}
 
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+  name: "Search",
+
+  props: {
+    currencies : Object 
+  },
+
+  data() {
+    return {
+      currencyToSearch: '',
+      currencyKey: '$'
+    }
+  },
+
+}
+</script>
+
+
+<template>
+
+  <div class="d-flex gap-3">
+    <div class="input-group">
+      <input type="number" class="form-control" step=0.00001 v-model="currencyToSearch">
+      <span class="input-group-text">{{ currencyKey }}</span>
+    </div>
+
+    <select class="form-select">
+      <option selected>...</option>
+      <option :value="key" v-for="(currency, key) in currencies">{{ currency }}</option>
+    </select>
+  </div>
+    
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
