eba0663d1bac626b75fd54548e4b6067e51c794f
aldoaquino451 <aldoaquino2909@gmail.com>
2024-05-08 15:47:57 +0200

    [add] Implemented disabling of currency selection based on the other selected currency

--- src/App.vue
+++ src/App.vue
@@ -30,19 +30,22 @@ export default {
         .then((res) => {
           const from = {};
           const to = {};
+
           from["amount"] = res.data.amount;
           from["currency"] = res.data.base;
+
           for (const [key, value] of Object.entries(res.data.rates)) {
             to["amount"] = value;
             to["currency"] = key;
           }
+
           this.data["up"] = position === "up" ? from : to;
           this.data["down"] = position === "up" ? to : from;
+
           this.data["result"] = {
             small: position,
             large: position === "up" ? "down" : "up",
           };
-          console.log(this.data);
         });
     },
     search(amount, currency, position) {
@@ -57,6 +60,7 @@ export default {
   },
 };
 </script>
+
 <template>
   <div class="my-container">
     <h1 class="text-center">Currency Converter</h1>
@@ -64,11 +68,17 @@ export default {
       <Result :data="this.data" />
     </div>
     <div class="d-flex flex-column gap-3">
-      <Search v-for="n in ['up', 'down']" :position="n" :data="this.data" :currencies="this.currencies"
-        @search="search" />
+      <Search
+        v-for="n in ['up', 'down']"
+        :position="n"
+        :data="this.data"
+        :currencies="this.currencies"
+        @search="search"
+      />
     </div>
   </div>
 </template>
+
 <style lang="scss">
 .my-container {
   max-width: 1000px;
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,25 +1,48 @@
 <script>
 export default {
   name: "Search",
+
   props: {
     currencies: Object,
     position: String,
     data: Object,
   },
+
+  computed: {
+    disabled() {
+      return this.position === "up" ? "down" : "up";
+    },
+  },
 };
 </script>
+
 <template>
   <div class="d-flex gap-3">
     <div class="input-group">
-      <input v-model="data[position].amount"
-        @keyup="$emit('search', data[position].amount, data[position].currency, position)" type="number" step="0.0001"
-        class="form-control" />
+      <input
+        v-model="data[position].amount"
+        @keyup="$emit('search', data[position].amount, data[position].currency, position)"
+        type="number"
+        step="0.0001"
+        class="form-control"
+      />
       <span class="input-group-text">$</span>
     </div>
-    <select v-model="data[position].currency"
-      @change="$emit('search', data[position].amount, data[position].currency, position)" class="form-select">
-      <option :value="short" v-for="(long, short) in currencies">{{ long }}</option>
+
+    <select
+      v-model="data[position].currency"
+      @change="$emit('search', data[position].amount, data[position].currency, position)"
+      class="form-select"
+    >
+      <option
+        :disabled="data[disabled].currency === short"
+        :value="short"
+        v-for="(long, short) in currencies"
+      >
+        {{ long }}
+      </option>
     </select>
   </div>
 </template>
+
 <style lang="scss" scoped></style>
